0bad0a9af8f9e78e87997e4b6d884b796bb84d2b
Yasiru Vithana <diwyanvithana@gmail.com>
2026-04-02 11:54:20 +0530

    feat: add create method to userService for user registration

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -3,6 +3,7 @@ import api from './api';
 export const userService = {
   getAll: (params) => api.get('/users', { params }),
   getById: (id) => api.get(`/users/${id}`),
+  create: (data) => api.post('/users', data),
   updateRole: (id, role) => api.patch(`/users/${id}/role`, { role }),
   getTechnicians: () => api.get('/users/technicians'),
   getMyProfile: () => api.get('/users/me/profile'),
