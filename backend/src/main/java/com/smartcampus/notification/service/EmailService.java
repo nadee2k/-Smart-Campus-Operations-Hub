d4f3efc764c59fe1496966558fbb6a1dd3e25c37
NipunDemintha <nipundemintha2018@gmail.com>
2026-04-27 15:46:26 +0530

    fix: add sendEmail and sendEmailOrThrow to EmailService interface and impl

--- backend/src/main/java/com/smartcampus/notification/service/EmailService.java
+++ backend/src/main/java/com/smartcampus/notification/service/EmailService.java
@@ -5,4 +5,8 @@ import com.smartcampus.booking.entity.Booking;
 public interface EmailService {
 
     void sendBookingApprovedEmail(Booking booking);
+
+    void sendEmail(String to, String subject, String body);
+
+    void sendEmailOrThrow(String to, String subject, String body);
 }
