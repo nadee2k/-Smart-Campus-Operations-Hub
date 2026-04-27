933d29255e1b7d4c7741f600019c42526b3e5644
NipunDemintha <nipundemintha2018@gmail.com>
2026-04-27 15:41:14 +0530

    fix: restore EmailService as interface to match EmailServiceImpl

--- backend/src/main/java/com/smartcampus/notification/service/EmailService.java
+++ backend/src/main/java/com/smartcampus/notification/service/EmailService.java
@@ -1,42 +1,8 @@
 package com.smartcampus.notification.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import com.smartcampus.booking.entity.Booking;
 
-@Service
-public class EmailService {
+public interface EmailService {
 
-    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
-    private final JavaMailSender mailSender;
-
-    @Value("${spring.mail.username:no-reply@smartcampus.local}")
-    private String fromEmail;
-
-    @Autowired
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendEmail(String to, String subject, String body) {
-        try {
-            sendEmailOrThrow(to, subject, body);
-            log.info("Email sent successfully to: {} from: {}", to, fromEmail);
-        } catch (Exception e) {
-            log.error("Failed to send email to: {} from: {}", to, fromEmail, e);
-        }
-    }
-
-    public void sendEmailOrThrow(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromEmail);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
-    }
+    void sendBookingApprovedEmail(Booking booking);
 }
